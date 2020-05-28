242d87158756653a7b85ce9705cce391199cf62e
am15h <amishgarg123@gmail.com>
2020-05-28 13:10:09 +0530

    add file_util methods

--- lib/src/common/file_util.dart
+++ lib/src/common/file_util.dart
@@ -1,3 +1,35 @@
-class FileUtil{
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 
+class FileUtil {
+  static Future<File> loadFileOnDevice(String fileAssetLocation) async {
+    final appDir = await getTemporaryDirectory();
+    final appPath = appDir.path;
+    final fileOnDevice = File('$appPath/$fileAssetLocation');
+    final rawAssetFile = await rootBundle.load('$fileAssetLocation');
+    final rawBytes = rawAssetFile.buffer.asUint8List();
+    await fileOnDevice.writeAsBytes(rawBytes, flush: true);
+    return fileOnDevice;
+  }
+
+  Future<Uint8List> loadFileAsBytes(String fileAssetLocation) async {
+    final rawAssetFile = await rootBundle.load('$fileAssetLocation');
+    final rawBytes = rawAssetFile.buffer.asUint8List();
+    return rawBytes;
+  }
+
+  static Future<List<String>> loadLabels(String fileAssetLocation) async {
+    final fileString = await rootBundle.loadString('$fileAssetLocation');
+    var list = <String>[];
+    final newLineList = fileString.split('\n');
+    for (var i = 0; i < newLineList.length; i++) {
+      var entry = newLineList[i].trim();
+      if (entry.length > 0) {
+        list.add(entry);
+      }
+    }
+    return list;
+  }
 }
